17068f4c28840ce359ba106097f54b6d4153f0d8
mcconkey <mcconkey@gmail.com>
2020-09-02 16:52:49 +0000

    added build folder

---
+++ build/precache-manifest.f4e63400902777bb8b9eaf3b68f8ce70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f96ab4538ad1dea62d7a881eb16a072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dac3f383fbfc6c1f6e8a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bc92cd8cb4fe91b42bf",
+    "url": "/static/js/2.075d05cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.075d05cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac3f383fbfc6c1f6e8a",
+    "url": "/static/js/main.32c81b11.chunk.js"
+  },
+  {
+    "revision": "5e6446f025a6f8c1ee83",
+    "url": "/static/js/runtime-main.f0c4ed1c.js"
+  }
+]);
